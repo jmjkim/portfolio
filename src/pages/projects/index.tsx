449f4af422721c4b73afff8036b50586ccb05530
Jack Kim <jmjkim00@icloud.com>
2023-04-24 17:48:52 -0700

    landing page for "projects" tab on navbar

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,18 +1,60 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import { ProjectData } from '../api/projects';
+import { useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
+import { InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+import Image from 'next/image';
+
 
 const LandingPageForProjects = ({ projects }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const router = useRouter()
+
+    const handleLinkClick = (title: string) => {
+        router.push(`/projects/${title}`)
+    }
+
+    useLayoutEffect(() => {
+        const ctx = gsap.context(() => {
+            gsap.fromTo(
+                ".project-container",
+                { 
+                    scale: 2,
+                },
+
+                { 
+                    scale: 1, 
+                    opacity: "100%",
+                    duration: 2,
+                },
+            );
+        });
+
+        return () => ctx.revert();
+    }, [])
+
     return (
         <>
             <div className="projects-navbar">
-                {projects.map(project => <Link key={project.title} href={`/${project.title}`}>{project.title}</Link>)}
+                {projects.map(project => {
+                    const projectMainImg = project.mainImage.replace("/public", "")
+
+                    return (
+                        <>
+                            <div className="project-container">
+                                <a key={project.title} onClick={() => handleLinkClick(project.title)}>
+                                    <span>{project.title}</span>
+                                    <div className="img-container">
+                                        <Image src={projectMainImg} alt={project.title} width={500} height={500} priority />
+                                    </div>
+                                </a>
+                            </div>
+                        </>
+                    )}
+                )}
             </div>
-            <button className="button" onClick={() => router.push('/')}>go back</button>
+            <div className="back-to-main-btn" onClick={() => router.push('/')}>Back to Main</div>
         </>
-        )
+    )
 }
 
 export default LandingPageForProjects
@@ -20,6 +62,6 @@ export default LandingPageForProjects
 export async function getServerSideProps() {
     const res = await fetch(`http://localhost:3000/api/projects`);
     const projects: ProjectData[] = await res.json();
-
+    
     return { props: { projects } };
 }
