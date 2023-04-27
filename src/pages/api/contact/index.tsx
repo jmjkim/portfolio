ff0d513c84aef235fb331b8100727b851dc35888
Jack Kim <jmjkim00@icloud.com>
2023-04-27 16:26:37 -0700

    latest code

---
+++ src/pages/api/contact/index.tsx
@@ -0,0 +1,9 @@
+const LandingPageForContact = () => {
+    return (
+        <div className="tally-form-wrapper">
+            <TallyForm />
+        </div>
+    )
+}
+
+export default LandingPageForContact
